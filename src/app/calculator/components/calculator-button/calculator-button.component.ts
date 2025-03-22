5afd30a51f33beb090415381845a3f79a93ac045
ludwingdiaz <faustinoludwin@gmail.com>
2025-03-22 19:31:23 +0100

    calculadora operadors + calculos + keybord calculos

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,9 +1,14 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
+  EventEmitter,
   HostBinding,
   input,
   OnInit,
+  Output,
+  signal,
+  viewChild,
   ViewEncapsulation,
 } from '@angular/core';
 
@@ -15,13 +20,21 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './calculator-button.component.css',
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]':'isDoubleSize()',
+    '[class.w-1/4]':'!isDoubleSize()'
     // attribute: 'hola',
     // 'data-size': 'XL',
   },
   // encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButtonComponent {
+
+  public isPressed = signal(false);
+
+  @Output() onClick: EventEmitter<string> = new EventEmitter<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+
   public isCommand = input(false, {
     transform: (value: boolean | string) =>
       typeof value === 'string' ? value === '' : value,
@@ -36,7 +49,31 @@ export class CalculatorButtonComponent {
   //   return this.isCommand();
   // }
 
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
+
+
+  handleClick() {
+    if (!this.contentValue()?.nativeElement) {
+      return;
+    }
+
+    const value = (this.contentValue()!.nativeElement.innerText);
+    console.log({ value });
+
+    this.onClick.emit(value.trim());
   }
+
+  public keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) return;
+
+    const value = this.contentValue()!.nativeElement.innerHTML;
+
+    if (value !== key) return;
+
+    this.isPressed.set(true);
+
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100)
+  }
+
 }
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, viewChildren } from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/service/calculator.service';
 
 @Component({
   selector: 'calculator',
@@ -7,10 +8,44 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  // styles: `
-  //   // .is-command {
-  //   //   @apply bg-indigo-700 bg-opacity-20;
-  //   // }
-  // `,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)'
+  }
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  private calculatorService = inject(CalculatorService);
+
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
+  public resultText = computed(() => this.calculatorService.resultText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
+  /*get resultText(){
+    return this.calculatorService.resultText();
+  }*/
+
+  handleClick(key: string) {
+    this.calculatorService.constructNumber(key);
+  }
+
+  /*@HostListener('document:keyup', ['$event'])*/
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      X: '*',
+      '/': '÷',
+      Enter: '=',
+    };
+
+
+    const key = event.key;
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue);
+
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyle(keyValue);
+    });
+  }
+}
